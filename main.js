6cf73458ee99c1d2bfc5a6be2272fc7258a48902
Einvi4 <132924013+Einvi4@users.noreply.github.com>
2023-05-11 13:41:51 -0400

    add functionality to the rock,paper,scissors game

--- main.js
+++ main.js
@@ -12,5 +12,68 @@ function getComputerChoice() {
     }else {
         return "scissors";
     }
-
 }
+
+function playRound(playerSelection,computerSelection) {
+    const playerLowerCase = playerSelection.toLowerCase();
+
+    if (computerSelection === "rock") {
+        if (playerLowerCase === "rock") {
+            return `Tie ${computerSelection} equals ${playerSelection}`;
+        }else if (playerLowerCase === "paper") {
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+        } else {
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        }
+    }else if (computerSelection === "paper") {
+        if (playerLowerCase === "rock") {
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        }else if (playerLowerCase === "paper") {
+            return `Tie ${computerSelection} equals ${playerSelection}`;
+        } else {
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+        }
+    } else {
+        if (playerLowerCase === "rock") {
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+        }else if (playerLowerCase === "paper") {
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+        } else {
+            return `Tie ${computerSelection} equals ${playerSelection}`;
+        }
+    }
+}
+
+
+function gameResult(playerResult, pcResult) {
+    if (playerResult > pcResult) {
+        return(`You Win. Player Score: ${playerResult} Computer Score: ${pcResult}`);
+    }else if (playerResult < pcResult) {
+        return (`You Lose. Player Score: ${playerResult} Computer Score: ${pcResult}`);
+    }else {
+        return (`The result is a tie. Player Score: ${playerResult} Computer Score: ${pcResult}`);
+    }
+}
+
+function game() {
+    let playerScore = 0;
+    let pcScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+
+        let computerSelection = getComputerChoice();
+        let playerSelection = prompt("Enter rock, paper or scissors:").toLowerCase();
+        let result = playRound(playerSelection, computerSelection);
+
+        if (result.includes("win")) {
+            playerScore++;
+        } else if (result.includes("lose")) {
+            pcScore++;
+        }
+        console.log(result);   
+        
+    }
+    console.log(gameResult(playerScore, pcScore));
+}
+
+game();
